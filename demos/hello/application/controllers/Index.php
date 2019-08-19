0dac13a602e3dfb10f35d488411ff8dd6532eb99
zhangyanxin <15201251947@163.com>
2019-08-19 16:34:57 +0800

    git add conf

--- demos/hello/application/controllers/Index.php
+++ demos/hello/application/controllers/Index.php
@@ -1,7 +1,8 @@
 <?php
 class IndexController extends Yaf_Controller_Abstract {
    // default action name
-   public function indexAction() {  
+   public function indexAction() { 
+   echo YAF_VERSION; 
         $this->getView()->content = "Hello World";
    }
 }
